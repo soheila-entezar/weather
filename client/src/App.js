7f2ea645d302289671440764ca31b44f9c1134b5
soheila-entezar <166860318+soheila-entezar@users.noreply.github.com>
2024-05-29 13:43:11 -0700

    create router dome and  client and tailwind css

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    
+       <BrowserRouter>
+      <Routes>
+        
+          
+       
+      </Routes>
+    </BrowserRouter>
+   
+  );
+}
+
+export default App;
