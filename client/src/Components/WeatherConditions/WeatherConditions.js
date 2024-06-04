441bf6fec66ddcea6cde6f1424968624128dba99
soheila-entezar <166860318+soheila-entezar@users.noreply.github.com>
2024-06-04 05:25:46 -0700

    fix blur

--- client/src/Components/WeatherConditions/WeatherConditions.js
+++ client/src/Components/WeatherConditions/WeatherConditions.js
@@ -5,7 +5,7 @@ import WeatherForecast from "../WeatherForecast/WeatherForecast";
 
 function WeatherConditions() {
   return (
-    <div className="w-[100%] sm:w-[40%] md:w-[30%] lg:w-[20%] h-[100%] mt-3 sm:mt-0 bg-white bg-opacity-20 rounded-lg md:rounded-r-lg   flex justify-center flex-wrap">
+    <div className="w-[100%] sm:w-[40%] md:w-[30%] lg:w-[20%] h-[100%] mt-3 sm:mt-0 bg-white bg-opacity-[0.1] rounded-lg md:rounded-r-lg   flex justify-center flex-wrap">
       <LocationSelector />
       {/* line */}
      <CurrentWeather />
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -4,7 +4,7 @@ import DailyWeatherOverview from '../Components/DailyWeatherOverview/DailyWeathe
 function Home() {
   return (
     <div className="backImage w-full min-h-screen flex justify-center items-center bg-cover bg-center relative overflow-auto">
-      <div className="w-full h-[100vh] bg-slate-600 bg-opacity-15  flex justify-center items-center overflow-auto">
+      <div className="w-full h-[100vh] bg-white bg-opacity-[0.1]  flex justify-center items-center overflow-auto">
         <div className="w-[95%] h-[90%] bg-white bg-opacity-45 flex justify-center items-center rounded-lg">
           <div className='w-[97%] md:w-[98%] lg:w-[99%] h-[98%] bg-cover bg-center backImage bg-opacity-100 rounded-md flex justify-end flex-wrap'>
             <DailyWeatherOverview />
