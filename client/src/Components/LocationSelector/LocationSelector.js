e3ff654b062f9f03f54d626c69f5c5cc24d62acb
soheila-entezar <166860318+soheila-entezar@users.noreply.github.com>
2024-06-10 10:26:45 -0700

    I made the locationSelected dynamic

--- client/src/Components/LocationSelector/LocationSelector.js
+++ client/src/Components/LocationSelector/LocationSelector.js
@@ -1,30 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GrLocation } from "react-icons/gr";
 import { SlArrowDown } from "react-icons/sl";
+
 function LocationSelector() {
+  const [option, setOption] = useState([
+    { id: 1, name: 'تبریز' },
+    { id: 2, name: 'مرند' },
+    { id: 3, name: 'اهر' },
+  ]);
+  const [selectedOption, setSelectedOption] = useState(option[0].name);
+
+  const handleChange = (event) => {
+    const newValue = event.target.value;
+    setSelectedOption(newValue);
+    alert(`شهر جدید: ${newValue}`);
+  };
+
   return (
     <div className="w-[70%] h-[5%] border-[0.6px] rounded-md border-stone-500 flex mt-2">
-      <div className=" flex-grow flex items-center justify-center border-r-[1px] border-stone-500 text-slate-50">
-          <SlArrowDown />
-        </div>
-        <div className="w-[70%] flex-grow flex items-center text-slate-50 justify-around   ">
-
-          
-         
-          <select className='w-9/12 appearance-none  outline-none pb-1 bg-transparent border-none'>
-             <option className="w-9/12 font-['yekan'] option-custom text-end">تبریز</option>
-             <option className="w-9/12 font-['yekan'] option-custom text-end">تبریز</option>
-             <option className="w-9/12 font-['yekan'] option-custom text-end">تبریز</option>
-          </select>
-           <div className="w-2/12 flex flex-grow justify-center">
-            <GrLocation />
-          </div>
-         
-        </div>
-
-        
+      <div className="flex-grow flex items-center justify-center border-r-[1px] border-stone-500 text-slate-50">
+        <SlArrowDown />
       </div>
-  )
+      <div className="w-[70%] flex-grow flex items-center text-slate-50 justify-around">
+        <select
+          className="w-9/12 appearance-none outline-none pb-1 bg-transparent border-none"
+          onChange={handleChange}
+          value={selectedOption}
+        >
+          {option.map((item) => (
+            <option
+              className="w-9/12 font-['yekan'] option-custom text-end"
+              key={item.id}
+              value={item.name}
+            >
+              {item.name}
+            </option>
+          ))}
+        </select>
+        <div className="w-2/12 flex flex-grow justify-center">
+          <GrLocation />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LocationSelector
+export default LocationSelector;
